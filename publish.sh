28f01d6a16bc43528c9c3e3b10acb92588b54360
David Denton <denton.david@gmail.com>
2015-05-09 22:20:38 +0100

    daveD - added idea stuff

--- publish.sh
+++ publish.sh
@@ -4,9 +4,9 @@
 
 export VERSION=$1
 
-git tag -a $version -m "released version $version to PyPi"
+git tag -a $VERSION -m "released version $VERSION to PyPi"
 git tag
-git push origin $version
+git push origin $VERSION
 
 python setup.py sdist upload -r pypi
 python setup.py bdist_wheel upload -r pypi
