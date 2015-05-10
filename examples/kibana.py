a3de3c3f0ae0a5aed784808cb8b416d9f8c5daa1
David Denton <denton.david@gmail.com>
2015-05-10 15:58:32 +0100

    daveD - example tweak

--- examples/kibana.py
+++ examples/kibana.py
@@ -5,7 +5,7 @@ filter = bd.Filter() \
     .with_field("application_name", "my app") \
     .with_field("environment", "production")
 
-fields = ['timestamp', 'http_code', 'response_time', 'path', '']
+fields = ['timestamp', 'http_code', 'response_time', 'path', 'host']
 
 dashboard = bd.Dashboard("My app Kibana dashboard").with_fields(fields).with_filter(filter).build()
 
