83ed3db82beaae2d076c01c27c1bb777c12b416f
David Denton <denton.david@gmail.com>
2016-07-26 15:02:08 +0100

    daveD - version 1.7.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name='balderdash',
-    version='1.7.0',
+    version='1.7.1',
     description='Builders for monitoring dashboards',
     url='http://github.com/daviddenton/balderdash/',
     license='Apache 2.0',
