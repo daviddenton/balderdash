645d9bf50780a72084bd780ead8f7852155a349f
Jamie Shiell <jamie@infernus.org>
2019-04-04 15:46:28 +0100

    Removed variables from constructor for consistency

--- balderdash/grafana.py
+++ balderdash/grafana.py
@@ -390,7 +390,7 @@ class Row:
 
 
 class Dashboard:
-    def __init__(self, title, variables=[]):
+    def __init__(self, title):
         self.title = title
         self.rows = []
         self.time = {
@@ -399,7 +399,7 @@ class Dashboard:
         }
         self.time_options = ["5m", "15m", "1h", "6h", "12h", "24h", "2d", "7d", "30d"]
         self.refresh_intervals = ["5s", "10s", "30s", "1m", "5m", "15m", "30m", "1h", "2h", "1d"]
-        self.variables = variables
+        self.variables = []
 
     def with_row(self, row):
         self.rows.append(row.build(len(self.rows) + 1))
